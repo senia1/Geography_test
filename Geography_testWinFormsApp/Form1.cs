720ee98fdf7a8ed377eaa5d8a63620ee6dae1209
senia <oks314@gmail.com>
2023-02-16 09:39:19 +0300

    WindowsFormsApp added

---
+++ Geography_testWinFormsApp/Form1.cs
@@ -0,0 +1,10 @@
+namespace Geography_testWinFormsApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
